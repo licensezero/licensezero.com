ef84f79163e7ddfe5e9257fb5d7e48e2a6a07d75
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-21 23:03:16 -0700

    Refactor

--- routes/stripe-redirect.js
+++ routes/stripe-redirect.js
@@ -1,5 +1,6 @@
 var FormData = require('form-data')
 var argon2 = require('argon2')
+var ecb = require('ecb')
 var encode = require('../data/encode')
 var fs = require('fs')
 var generateKeypair = require('../data/generate-keypair')
@@ -11,6 +12,7 @@ var niceware = require('niceware')
 var parseJSON = require('json-parse-errback')
 var path = require('path')
 var runWaterfall = require('run-waterfall')
+var simpleConcat = require('simple-concat')
 var stripeNoncePath = require('../paths/stripe-nonce')
 var uuid = require('uuid/v4')
 
@@ -19,13 +21,13 @@ module.exports = function (request, response, service) {
     response.statusCode = 405
     response.end()
   } else {
-    response.setHeader('Content-Type', 'text/html')
     var query = request.query
     if (query.error) {
       request.log.info({
         error: query.error,
         description: query.error_description
       }, 'connect error')
+      response.setHeader('Content-Type', 'text/html')
       response.end(html`
 <!doctype html>
 <html lang=en>
@@ -48,105 +50,134 @@ module.exports = function (request, response, service) {
     ) {
       var nonce = query.state
       var nonceFile = stripeNoncePath(service, nonce)
-      fs.readFile(nonceFile, 'utf8', function (error, read) {
-        if (error) {
-          if (error.code === 'ENOENT') {
-            response.statusCode = 400
-            response.end()
-          } else {
-            response.statusCode = 500
-            response.end()
-          }
-        } else {
-          parseJSON(read, function (error, nonceData) {
-            if (error) {
-              response.statusCode = 500
-              response.end()
-            } else if (
-              ['name', 'email', 'jurisdiction', 'timestamp']
-                .some(function (key) {
-                  return !isString(nonceData[key])
-                })
-            ) {
-              request.log.error(nonceData, 'invalid stripe nonce file')
-              response.statusCode = 500
-              response.end()
+      runWaterfall([
+        function readNonceFile (done) {
+          fs.readFile(nonceFile, function (error, read) {
+            if (error.code === 'ENOENT') {
+              error.statusCode = 404
             } else {
-              var id = uuid()
-              var licensorFile = licensorPath(service, id)
-              var form = new FormData()
-              form.append('client_secret', service.stripe.secret)
-              form.append('code', query.code)
-              form.append('grant_type', 'authorization_code')
-              form.pipe(
-                https.request({
-                  method: 'POST',
-                  host: 'connect.stripe.com',
-                  path: '/oauth/token'
+              error.statusCode = 500
+            }
+            done(error, read)
+          })
+        },
+        function deleteNonceFile (done, read) {
+          fs.unlink(nonceFile, ecb(done, function (error) {
+            done(null, read)
+          }))
+        },
+        function parseNonceFile (buffer, done) {
+          parseJSON(buffer, function (error, parsed) {
+            if (error) {
+              error.statusCode = 400
+            }
+            done(error, parsed)
+          })
+        },
+        function validateNonceFile (nonceData, done) {
+          if (
+            ['name', 'email', 'jurisdiction', 'timestamp']
+              .some(function (key) {
+                return isString(nonceData[key])
+              })
+          ) {
+            done(null, nonceData)
+          } else {
+            var error = new Error('invalid nonce file')
+            error.statusCode = 500
+            done(error)
+          }
+        },
+        function requestStripeCredentials (nonceData, done) {
+          var form = new FormData()
+          form.append('client_secret', service.stripe.secret)
+          form.append('code', query.code)
+          form.append('grant_type', 'authorization_code')
+          form.pipe(
+            https.request({
+              method: 'POST',
+              host: 'connect.stripe.com',
+              path: '/oauth/token'
+            })
+              .once('error', function (error) {
+                request.log.error(error)
+                response.statusCode = 500
+                response.end()
+              })
+              .once('response', function (response) {
+                simpleConcat(response, ecb(done, function (buffer) {
+                  parseJSON(buffer, ecb(done, function (parsed) {
+                    done(null, parsed, nonceData)
+                  }))
+                }))
+              })
+          )
+        },
+        function validateStripeBody (body, nonceData, done) {
+          if (body.error) {
+            request.log.error(body, 'stripe error')
+            body.error.statusCode = 500
+            done(body.error)
+          } else if (
+            ['stripe_user_id', 'refresh_token']
+              .every(function (key) {
+                return isString(body[key])
+              })
+          ) {
+            request.log.error(body, 'invalid stripe body')
+            var error = new Error('invalid stripe body')
+            error.statusCode = 500
+            done(error)
+          } else {
+            done(null, body, nonceData)
+          }
+        },
+        function writeLicensorFile (stripeData, nonceData, done) {
+          var id = uuid()
+          var licensorFile = licensorPath(service, id)
+          var keypair = generateKeypair()
+          var passphrase = niceware(16).join(' ')
+          runWaterfall([
+            mkdirp.bind(null, path.dirname(licensorFile)),
+            function hashPassphrase (done) {
+              argon2.hash(passphrase)
+                .catch(done)
+                .then(function (result) {
+                  done(null, result)
                 })
-                  .once('error', function (error) {
-                    request.log.error(error)
-                    response.statusCode = 500
-                    response.end()
-                  })
-                  .once('response', function (response) {
-                    parseJSON(response, function (error, stripe) {
-                      if (error) {
-                        request.log.error(error)
-                        response.statusCode = 500
-                        response.end()
-                      } else if (stripe.error) {
-                        request.log.error(stripe)
-                        response.statusCode = 500
-                        response.end()
-                      } else if (
-                        ['stripe_user_id', 'refresh_token']
-                          .every(function (key) {
-                            return !isString(stripe[key])
-                          })
-                      ) {
-                        request.log.error(stripe)
-                        response.statusCode = 500
-                        response.end()
-                      } else {
-                        var keypair = generateKeypair()
-                        var passphrase = niceware(16).join(' ')
-                        runWaterfall([
-                          mkdirp.bind(null, path.dirname(licensorFile)),
-                          function hashPassphrase (done) {
-                            argon2.hash(passphrase)
-                              .catch(done)
-                              .then(function (result) {
-                                done(null, result)
-                              })
-                          },
-                          function writeLicensorFile (done, hash) {
-                            fs.writeFile(
-                              licensorFile,
-                              JSON.stringify({
-                                name: nonceData.name,
-                                email: nonceData.email,
-                                jurisdiction: nonceData.jurisdiction,
-                                registered: new Date().toISOString(),
-                                password: hash,
-                                keypair: {
-                                  publicKey: encode(keypair.publicKey),
-                                  privateKey: encode(keypair.privateKey)
-                                },
-                                stripe: {
-                                  id: stripe.stripe_user_id,
-                                  refresh: stripe.refresh_token
-                                }
-                              }),
-                              done
-                            )
-                          }
-                        ], function (error) {
-                          if (error) {
-                            response.statusCode = 500
-                            response.end()
-                          } else {
-                            response.end(html`
+            },
+            function writeLicensorFile (done, hash) {
+              fs.writeFile(
+                licensorFile,
+                JSON.stringify({
+                  name: nonceData.name,
+                  email: nonceData.email,
+                  jurisdiction: nonceData.jurisdiction,
+                  registered: new Date().toISOString(),
+                  password: hash,
+                  keypair: {
+                    publicKey: encode(keypair.publicKey),
+                    privateKey: encode(keypair.privateKey)
+                  },
+                  stripe: {
+                    id: stripeData.stripe_user_id,
+                    refresh: stripeData.refresh_token
+                  }
+                }),
+                done
+              )
+            }
+          ], ecb(done, function () {
+            done(null, id, passphrase)
+          }))
+        }
+      ], function (error) {
+        if (error) {
+          response.statusCode = error.statusCode || 500
+          response.end()
+        } else {
+          response.setHeader('Content-Type', 'text/html')
+          response.end(html`
 <!doctype html>
 <html lang=en>
 <head>
@@ -167,15 +198,7 @@ module.exports = function (request, response, service) {
 l0 authenticate "${id}" "${password}"</code></pre>
 </body>
 </html>
-                            `)
-                          }
-                        })
-                      }
-                    })
-                  })
-              )
-            }
-          })
+          `)
         }
       })
     } else {
