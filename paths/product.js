401cb1f430d545c80c591abe8530cec1d5e573b5
Kyle E. Mitchell <kyle@kemitchell.com>
2017-08-22 20:35:01 -0700

    Create a directory per product

--- paths/product.js
+++ paths/product.js
@@ -2,5 +2,5 @@ var licensorPath = require('./licensor')
 var path = require('path')
 
 module.exports = function (service, product) {
-  return path.join(service.directory, 'products', product)
+  return path.join(service.directory, 'products', product, 'product')
 }
