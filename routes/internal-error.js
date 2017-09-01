c455a611539bee8622e5653a10c5888dfb621242
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-01 01:08:02 -0700

    Template 500 HTML responses like other pages

--- routes/internal-error.js
+++ routes/internal-error.js
@@ -1,4 +1,8 @@
+var footer = require('./partials/footer')
+var head = require('./partials/head')
+var header = require('./partials/header')
 var html = require('./html')
+var nav = require('./partials/nav')
 
 module.exports = /* istanbul ignore next */ function (response, error) {
   response.statusCode = 500
@@ -6,17 +10,17 @@ module.exports = /* istanbul ignore next */ function (response, error) {
   response.end(html`
 <!doctype html>
 <html lang=en>
-<head>
-  <meta charset=UTF-8>
-  <title>License Zero | Error<title>
-  <link rel=stylesheet href=/normalize.css>
-  <link rel=stylesheet href=/styles.css>
-</head>
+${head('Error')}
 <body>
-  <h1>Server Error</h2>
-  <p>
-    The website ran into an unexpected technical error.
-  </p>
+  ${nav()}
+  ${header()}
+  <main>
+    <h1>Server Error</h2>
+    <p>
+      The website ran into an unexpected technical error.
+    </p>
+  </main>
+  ${footer()}
 </body>
 </html>
   `)
