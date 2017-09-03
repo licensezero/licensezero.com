59fe003323b6fd252a5a0f48b840b7cf19ecbe2e
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-03 14:00:51 -0700

    Remove US-embargoed countries from jurisdictions

--- data/jurisdictions.js
+++ data/jurisdictions.js
@@ -1,7 +1,19 @@
 var data = require('iso-3166-2').data
 
+var EMBARGOES = [
+  'CU', // Cuba
+  'IR', // Iran
+  'KP', // North Korea
+  'LB', // Lebanon
+  'LY', // Libya
+  'SD', // Sudan
+  'SO', // Somalia
+  'SY' // Syria
+]
+
 module.exports = Object.keys(data)
   .reduce(function (codes, country) {
+    if (EMBARGOES.includes(country)) return codes
     return codes.concat(
       Object.keys(data[country].sub)
         .reduce(function (codes, subdivision) {
