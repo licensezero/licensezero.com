b515979b910efc4d6a8c8e0a7d6a1cf243f10777
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-10 23:00:52 -0700

    Set viewport

--- routes/partials/head.js
+++ routes/partials/head.js
@@ -5,6 +5,7 @@ module.exports = function (subtitle) {
   return html`
 <head>
   <meta charset=UTF-8>
+  <meta name=viewport content="width=725">
   <title>License Zero${subtitle && escape(' // ' + subtitle)}</title>
   <link rel=stylesheet href=/normalize.css>
   <link rel=stylesheet href=/styles.css>
