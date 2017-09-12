78da1433fd344f6e6130bc670d144f28c685e8f2
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-11 21:42:08 -0700

    Strike "perpetual, with upgrades"

--- routes/pay.js
+++ routes/pay.js
@@ -104,9 +104,7 @@ ${head('Buy Licenses')}
                     order.tier === 'solo'
                       ? 'one user'
                       : TIERS[order.tier] + ' users'
-                  },
-                  perpetual,
-                  with upgrades
+                  }
                 </p>
               </td>
               <td class=price>
