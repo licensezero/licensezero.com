e7c4bde69085c1c3184cfac3c6b1837d6e7b54e9
Kyle E. Mitchell <kyle@kemitchell.com>
2017-09-14 19:16:25 -0700

    Increase maximum price to $3,000

--- routes/actions/common/price.js
+++ routes/actions/common/price.js
@@ -2,5 +2,5 @@ module.exports = {
   description: 'price per license, in United States cents',
   type: 'integer',
   min: 300, // 3 dollars
-  max: 100000 // 1,000 dollars
+  max: 300000 // 3,000 dollars
 }
