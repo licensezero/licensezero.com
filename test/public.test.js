fb2c66823ee28b3749f3a0c24c970548d3f8b24e
Kyle E. Mitchell <kyle@kemitchell.com>
2017-11-22 17:33:03 -0800

    Test public license choice metadata

--- test/public.test.js
+++ test/public.test.js
@@ -10,7 +10,7 @@ var tape = require('tape')
 var uuid = require('uuid/v4')
 var writeTestLicensor = require('./write-test-licensor')
 
-tape('public', function (test) {
+tape('public noncommercial', function (test) {
   server(function (port, service, close) {
     var projectID
     runSeries([
@@ -81,6 +81,167 @@ tape('public', function (test) {
           test.assert(
             licensezero.hasOwnProperty('agentSignature'),
             'licensezero.agentSignature'
+          )
+          test.assert(
+            licensezero.hasOwnProperty('license'),
+            'licensezero.license'
+          )
+          test.assert(
+            licensezero.license.hasOwnProperty('terms'),
+            'licensezero.license.terms'
+          )
+          test.equal(
+            licensezero.license.terms, 'noncommercial',
+            'noncommercial terms'
+          )
+          test.equal(
+            metadata.license, 'SEE LICENSE IN LICENSE',
+            'metadata.license'
+          )
+          apiRequest(port, {
+            action: 'key'
+          }, function (error, response) {
+            if (error) return done(error)
+            var agentPublicKey = response.key
+            // Validate license signatures.
+            test.assert(
+              ed25519.verify(
+                license.document,
+                license.licensorSignature,
+                LICENSOR.publicKey
+              ),
+              'valid document licensor signature'
+            )
+            test.assert(
+              ed25519.verify(
+                license.document +
+                '---\nLicensor:\n' +
+                [
+                  license.licensorSignature.slice(0, 32),
+                  license.licensorSignature.slice(32, 64),
+                  license.licensorSignature.slice(64, 96),
+                  license.licensorSignature.slice(96)
+                ].join('\n') + '\n',
+                license.agentSignature,
+                agentPublicKey
+              ),
+              'valid document agent signature'
+            )
+            // Validate metadata signatures.
+            test.assert(
+              ed25519.verify(
+                stringify(licensezero.license),
+                licensezero.licensorSignature,
+                LICENSOR.publicKey
+              ),
+              'valid metadata licensor signature'
+            )
+            test.assert(
+              ed25519.verify(
+                stringify({
+                  license: licensezero.license,
+                  licensorSignature: licensezero.licensorSignature
+                }),
+                licensezero.agentSignature,
+                agentPublicKey
+              ),
+              'valid metadata agent signature'
+            )
+            done()
+          })
+        })
+      }
+    ], function (error) {
+      test.error(error, 'no error')
+      test.end()
+      close()
+    })
+  })
+})
+
+tape('public reciprocal', function (test) {
+  server(function (port, service, close) {
+    var projectID
+    runSeries([
+      writeTestLicensor.bind(null, service),
+      function offer (done) {
+        apiRequest(port, Object.assign(clone(OFFER), {
+          licensorID: LICENSOR.id,
+          token: LICENSOR.token
+        }), function (error, response) {
+          if (error) return done(error)
+          test.equal(response.error, false, 'error false')
+          projectID = response.projectID
+          done()
+        })
+      },
+      function publicLicense (done) {
+        apiRequest(port, {
+          action: 'public',
+          licensorID: LICENSOR.id,
+          token: LICENSOR.token,
+          projectID: projectID,
+          terms: 'reciprocal'
+        }, function (error, response) {
+          if (error) return done(error)
+          test.equal(
+            response.error, false,
+            'error false'
+          )
+          test.assert(
+            response.hasOwnProperty('license'),
+            'document'
+          )
+          var license = response.license
+          test.assert(
+            license.hasOwnProperty('document'),
+            'license.document'
+          )
+          test.assert(
+            license.hasOwnProperty('licensorSignature'),
+            'license.licensorSignature'
+          )
+          test.assert(
+            /^[0-9a-f]{128}$/.test(license.licensorSignature),
+            'licensor ed25519 signature'
+          )
+          test.assert(
+            license.hasOwnProperty('agentSignature'),
+            'license.agentSignature'
+          )
+          test.assert(
+            /^[0-9a-f]{128}$/.test(license.agentSignature),
+            'agent ed25519 signature'
+          )
+          test.assert(
+            response.hasOwnProperty('metadata'),
+            'metadata'
+          )
+          var metadata = response.metadata
+          test.assert(
+            metadata.hasOwnProperty('licensezero'),
+            'licensezero'
+          )
+          var licensezero = metadata.licensezero
+          test.assert(
+            licensezero.hasOwnProperty('licensorSignature'),
+            'licensezero.licensorSignature'
+          )
+          test.assert(
+            licensezero.hasOwnProperty('agentSignature'),
+            'licensezero.agentSignature'
+          )
+          test.assert(
+            licensezero.hasOwnProperty('license'),
+            'licensezero.license'
+          )
+          test.assert(
+            licensezero.license.hasOwnProperty('terms'),
+            'licensezero.license.terms'
+          )
+          test.equal(
+            licensezero.license.terms, 'reciprocal',
+            'reciprocal terms'
           )
           test.equal(
             metadata.license, 'SEE LICENSE IN LICENSE',
