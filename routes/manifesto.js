ece4c0c39362f143e81dfece810e3e2125dfbd8b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-02-12 20:48:10 -0800

    Redirect to blog for manifesto

--- routes/manifesto.js
+++ routes/manifesto.js
@@ -1,48 +1,7 @@
-var commonmark = require('commonmark')
-var fs = require('fs')
-var html = require('./html')
-var internalError = require('./internal-error')
-var path = require('path')
-
-var footer = require('./partials/footer')
-var head = require('./partials/head')
-var header = require('./partials/header')
-var nav = require('./partials/nav')
-
-var content
-
-var options = {
-  smart: true,
-  safe: true
-}
+var BLOG_POST = 'https://blog.licensezero.com/2017/09/12/manifesto.html'
 
 module.exports = function (request, response, service) {
-  var file = path.join(__dirname, 'manifesto.md')
-  if (content) {
-    withContent()
-  } else {
-    fs.readFile(file, 'utf8', function (error, markdown) {
-      if (error) return internalError(response, error)
-      var reader = new commonmark.Parser(options)
-      var writer = new commonmark.HtmlRenderer(options)
-      var parsed = reader.parse(markdown)
-      content = writer.render(parsed)
-      withContent()
-    })
-  }
-  function withContent () {
-    response.setHeader('Content-Type', 'text/html; charset=UTf-8')
-    response.end(html`
-<!doctype html>
-<html lang=EN>
-${head('Manifesto')}
-<body>
-  ${nav()}
-  ${header()}
-  <main>${content}</main>
-  ${footer()}
-</body>
-</html>
-    `)
-  }
+  response.statusCode = 301
+  response.setHeader('Location', BLOG_POST)
+  response.end()
 }
