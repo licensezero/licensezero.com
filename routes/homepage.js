4d9159b8ef661e51659c414faeb197150429ff1a
Kyle E. Mitchell <kyle@kemitchell.com>
2018-02-13 01:07:13 -0800

    Revise homepage

--- routes/homepage.js
+++ routes/homepage.js
@@ -8,19 +8,20 @@ var nav = require('./partials/nav')
 var LICENSOR = [
   'npm install -g licensezero',
   '# Installs the License Zero command-line interface',
-  'licensezero identify "Anna Maintainer" US-TX anna@example.com',
+  'l0 identify "Anna Maintainer" US-TX anna@example.com',
   '# Configures the CLI for for Anna, a developer in Texas.',
-  'licensezero register',
-  '# Provides a link to connect a Stripe account receive payments, and',
-  '# an access token once the Stripe account is connected.',
-  'licensezero set-licensor-id $NEW_LICENSOR_UUID',
-  '# Prompts for the new new licensor\'s access token, to save for use',
-  '# offering projects for license.',
+  'l0 register',
+  '# Provides a link to connect a Stripe account receive payments,',
+  '# and an access token once the Stripe account is connected.',
+  'l0 set-licensor-id $NEW_LICENSOR_UUID',
+  '# Prompts for the new new licensor\'s access token, to save',
+  '# for use offering projects for license.',
   'cd a-node-project',
-  'licensezero offer 500',
+  'l0 offer 500',
   '# Offer private licenses through licensezero.com at $5 US.',
-  'licensezero license --noncommercial $NEW_PROJECT_UUID',
-  '# Writes LICENSE and package.json metadata for licensezero quote to read.',
+  'l0 license --noncommercial $NEW_PROJECT_UUID',
+  '# Writes LICENSE and package.json metadata for',
+  '# `l0 quote` to read.',
   'git add LICENSE package.json',
   'git commit -m "License Zero"'
 ]
@@ -28,24 +29,24 @@ var LICENSOR = [
 var LICENSEE = [
   'npm install -g licensezero',
   '# Installs the License Zero command-line interface.',
-  'licensezero identify "Larry Licensor" US-CA larry@example.com',
+  'l0 identify "Larry Licensor" US-CA larry@example.com',
   '# Configures the CLI for Larry, a developer in California.',
   'cd a-node-project',
-  'licensezero quote',
+  'l0 quote',
   '# Lists License Zero dependencies in node_modules',
   '# and the costs of missing licenses.',
-  'licensezero buy',
+  'l0 buy',
   '# Opens an online order page for all needed licenses.',
-  'licensezero import-bundle $ORDER_BUNDLE_URL',
+  'l0 import-bundle $ORDER_BUNDLE_URL',
   '# Imports a bundle of licenses from the order page.'
 ]
 
 var SPONSOR = [
   'npm install -g licensezero',
   '# Installs the License Zero command-line interface.',
-  'licensezero identify "Sam Sponsor" US-NY sam@example.com',
+  'l0 identify "Sam Sponsor" US-NY sam@example.com',
   '# Configures the CLI for Sam, a developer in New York.',
-  'licensezero sponsor $PROJECT_ID',
+  'l0 sponsor $PROJECT_ID',
   '# Opens an online order page for sponsoring relicense',
   '# of a project onto License Zero Permissive terms.'
 ]
@@ -61,43 +62,25 @@ ${head()}
   ${header()}
   <main>
     <p class=lead>
-      License Zero is a shot across the bow of Open Source software
-      entitlement, free-as-in-worthless, and maintainer disposability.
+      License Zero is a new way to support open software developers.
     </p>
     <p>
-      Feel overwhelmed by business-driven demand for open software,
-      maintenance, and support?  License Zero offers
-      all the
-      <a href=https://github.com/licensezero/licensezero-cli>tools</a>
-      <a href=/licenses>you<a/>
-      <a href=/terms>need</a>
-      to go in business yourself,
-      and start receiving compensation for your contributions.
-    </p>
-    <p>
-      Tired of seeing your open code end up in closed systems, with no
-      contribution or support flowing back?
-      License Zero
-      <a href=/licenses/reciprocal>puts the teeth back in copyleft</a>,
-      requiring community members to give back to Open Source or support
-      those who do.
-    </p>
-    <p>
-      Want a simple, low-friction way to support maintainers of open
-      code you use and rely on, when you can&rsquo;t give back in kind?
-      License Zero makes it easy,
-      <a href=#users>right from the command line</a>.
+      Developers can choose from two new licenses that make their work
+      free for noncommercial or open-source users, then sell private
+      licenses to other devs who want to use commercially or in
+      closed source.  Everything happens through a simple, dev-friendly
+      interface.
     </p>
     <h2 id=users>Users</h2>
     <p>
       Noncommercial users of <a href=/licenses/noncommercial>noncommercial</a> projects,
-      and Open Source users of <a href=/licenses/reciprocal>reciprocal</a> projects,
+      and open source users of <a href=/licenses/reciprocal>reciprocal</a> projects,
       don&rsquo;t need any special permission to use License Zero
-      software. For them, License Zero code works like Open Source under
+      software. For them, License Zero code works like open source under
       two-clause BSD, MIT, or a similarly permissive license.
     </p>
     <p>
-      Customers who want permission for commercial or non-Open Source
+      Users who want permission for commercial or non-open source
       uses can identify, price, and buy licenses for all License
       Zero dependencies of their Node.js projects, in one checkout
       transaction, using a free command-line tool:
@@ -107,15 +90,13 @@ ${head()}
     <p>
       In a perfect world, making software wouldn&rsquo;t cost anything,
       and neither would using it, no strings attached.
-    </p>
-    <p>
       License Zero maintainers can offer to bring their software closer
       to that perfect world, by relicensing, for a fee, on permissive
       terms. Sponsors can accept those offers just like
       they can buy licenses:
     </p>
     <pre class=terminal>${formatSession(SPONSOR)}</pre>
-    <h2>Maintainers</h2>
+    <h2>Developers</h2>
     <p>
       On the maintainer side, it&rsquo;s quick and easy to create,
       license, and price projects, with just a few short commands:
