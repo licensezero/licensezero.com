648b7a0b6df71ac1adbe71f2f5af8084a00b082f
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-12 00:36:44 -0700

    Test /install.sh

---
+++ test/install.test.js
@@ -0,0 +1,22 @@
+var http = require('http')
+var server = require('./server')
+var tape = require('tape')
+
+tape('GET /install.sh', function (test) {
+  server(function (port, close) {
+    http.request({port: port, path: '/install.sh'})
+      .once('error', function (error) {
+        test.error(error, 'no error')
+        finish()
+      })
+      .once('response', function (response) {
+        test.equal(response.statusCode, 302, '302')
+        finish()
+      })
+      .end()
+    function finish () {
+      test.end()
+      close()
+    }
+  })
+})
