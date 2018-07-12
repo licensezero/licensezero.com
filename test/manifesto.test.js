42879392469d92bd364bf5f449d84f735ba4cd70
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-12 00:38:37 -0700

    Test /manifesto

---
+++ test/manifesto.test.js
@@ -0,0 +1,22 @@
+var http = require('http')
+var server = require('./server')
+var tape = require('tape')
+
+tape('GET /manifesto', function (test) {
+  server(function (port, close) {
+    http.request({port: port, path: '/manifesto'})
+      .once('error', function (error) {
+        test.error(error, 'no error')
+        finish()
+      })
+      .once('response', function (response) {
+        test.equal(response.statusCode, 301, '301')
+        finish()
+      })
+      .end()
+    function finish () {
+      test.end()
+      close()
+    }
+  })
+})
