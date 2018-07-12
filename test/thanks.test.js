5428c1f1ad3884ecadd8a318328e79d613a840d4
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-12 00:44:16 -0700

    Test /thanks

---
+++ test/thanks.test.js
@@ -0,0 +1,31 @@
+var http = require('http')
+var server = require('./server')
+var simpleConcat = require('simple-concat')
+var tape = require('tape')
+
+tape('GET /thanks', function (test) {
+  server(function (port, close) {
+    http.request({port: port, path: '/thanks'})
+      .once('error', function (error) {
+        test.error(error, 'no error')
+        finish()
+      })
+      .once('response', function (response) {
+        test.equal(response.statusCode, 200, '200')
+        simpleConcat(response, function (error, body) {
+          test.error(error, 'no body error')
+          body = body.toString()
+          test.assert(
+            body.includes('Forrest'),
+            'includes "Forrest"'
+          )
+          finish()
+        })
+      })
+      .end()
+    function finish () {
+      test.end()
+      close()
+    }
+  })
+})
