577ff25f6efd9bd4012e0d6b5df7acd4e0680a0b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-09-05 22:25:17 -0700

    Check for required environment variables on start

--- server.js
+++ server.js
@@ -11,6 +11,31 @@ var log = pino({name: 'licensezero.com'})
 
 log.info({event: 'data', directory: process.env.DIRECTORY})
 
+// Check required environment variables.
+var requiredEnvironmentVariables = [
+  'NODE_ENV',
+  'MAILGUN_KEY',
+  'MAILGUN_DOMAIN',
+  'MAILGUN_FROM',
+  'STRIPE_PUBLISHABLE_KEY',
+  'STRIPE_PRIVATE_KEY',
+  'STRIPE_SECRET_KEY',
+  'STRIPE_WEBHOOK_SECRET',
+  'STRIPE_APPLICATION',
+  'STRIPE_CLIENT_ID',
+  'DIRECTORY',
+  'PORT',
+  'PUBLIC_KEY',
+  'PRIVATE_KEY',
+  'COMMISSION'
+]
+requiredEnvironmentVariables.forEach(function (key) {
+  if (!process.env[key]) {
+    log.error({key: key}, 'missing environment variable')
+    process.exit(1)
+  }
+})
+
 // Create the HTTP server.
 var requestHandler = makeHandler(log)
 var server = http.createServer(requestHandler)
