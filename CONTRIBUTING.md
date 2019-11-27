d070610eb6b89440417038341aaf2cb85fc722e7
Kyle E. Mitchell <kyle@kemitchell.com>
2019-11-26 21:27:28 -0800

    Support customized offer pages

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,3 +3,5 @@ This project uses [JavaScript Standard Style](https://standardjs.com/).  You can
 If you would like to submit a pull request, please be prepared to license your contributions under [Charity](https://licensezero.com/licenses/charity) terms, a modern evolution of licenses like MIT and the two-clause BSD license.
 
 Before starting work on a potentially large or significant patch, please consider [opening an issue](https://github.com/licensezero/licensezero.com/issues/new) to discuss work you would like to do.  Your input and contributions are _very_ welcome.  I want to make sure you have the latest information on plans for development, before you spend significant time.
+
+If you're a License Zero developer looking to customize the page for your offer, copy `customizations/example.html` to `customizations/$id.html` and edit to suit.  Feel free to open a work-in-progress pull request and ask for help.
--- routes/offer.js
+++ routes/offer.js
@@ -1,14 +1,20 @@
+var JURISDICTIONS = require('licensezero-jurisdictions')
 var PERSON = require('./actions/common/person').const
 var UUID = new RegExp(require('../data/uuidv4-pattern'))
 var escape = require('./escape')
 var footer = require('./partials/footer')
 var formatPrice = require('../util/format-price')
+var fs = require('fs')
+var handlebars = require('handlebars')
 var head = require('./partials/head')
 var header = require('./partials/header')
 var html = require('./html')
+var internalError = require('./internal-error')
+var iso31662 = require('iso-3166-2')
 var jurisdictionOptions = require('./partials/jurisdiction-options')
 var nav = require('./partials/nav')
 var notFound = require('./not-found')
+var path = require('path')
 var readProject = require('../data/read-project')
 var renderJurisdiction = require('./partials/jurisdiction')
 var sanitizeProject = require('../data/sanitize-project')
@@ -24,68 +30,118 @@ module.exports = function (request, response) {
     if (error) return notFound(request, response, error)
     sanitizeProject(project)
     var licensor = project.licensor
-    response.setHeader('Content-Type', 'text/html; charset=UTf-8')
-    response.end(html`
-<!doctype html>
-<html lang=EN>
-  ${head(projectID, {
-    title: licensor.name + '’s Project',
-    description: project.description
-  })}
-  <body>
-    ${nav()}
-    ${header()}
-    <main>
-      <h2>Project</h2>
-      <section>
-        <dl>
-          <dt>Description</dt>
-          <dd class=description>${escape(project.description)}</dd>
-          <dt>Homepage</dt>
-          <dd class=homepage>
-            <a href="${escape(project.homepage)}" target=_blank>
-              ${escape(project.homepage)}
-            </a>
-          </dd>
-          <dt>Project ID</dt>
-          <dd>
-            <code class=projectID>${escape(projectID)}</code>
-            <button class=clipboard data-clipboard-text="${escape(projectID)}">Copy to Clipboard</button>
-          </dd>
-        </dl>
-      </section>
-      <section>
-        <h3>Licensor</h3>
-        <dl>
-          <dt>Name</dt>
-          <dd>${escape(licensor.name)}</dd>
-          <dt>Jurisdiction</dt>
-          <dd>${renderJurisdiction(licensor.jurisdiction)}</dd>
-          <dt>Public Signing Key</dt>
-          <dd>
-            <pre><code>${
-  licensor.publicKey.slice(0, 32) + '\n' +
-              licensor.publicKey.slice(32)
-}</code></pre>
-            <button class=clipboard data-clipboard-text="${escape(licensor.publicKey)}">Copy to Clipboard</button>
-          </dd>
-        </dl>
-      </section>
-      <h3>Pricing</h3>
-      ${
-  project.retracted
-    ? retracted()
-    : priceList(project)
-}
-      ${orderForm(project)}
-    </main>
-    ${footer()}
-    <script src=/clipboard.min.js></script>
-    <script>new ClipboardJS('.clipboard')</script>
-  </body>
-</html>
-    `)
+    licensor.jurisdiction = renderJurisdiction(licensor.jurisdiction)
+    var data = { project, licensor }
+    var customizationPath = path.join(
+      __dirname, '..', 'customizations', projectID + '.html'
+    )
+    fs.readFile(
+      customizationPath, 'utf8',
+      function (error, template) {
+        if (error) {
+          if (error.code === 'ENOENT') return sendHTML(data)
+          return internalError(request, response, error)
+        }
+        if (template.length === 0) {
+          return internalError(request, response, error)
+        }
+        var compiled = handlebars.compile(template)
+        response.setHeader('Content-Type', 'text/html; charset=UTf-8')
+        try {
+          data.jurisdictions = JURISDICTIONS.map(function (code) {
+            var parsed = iso31662.subdivision(code)
+            return {
+              country: parsed.countryName,
+              subdivision: parsed.name,
+              code
+            }
+          })
+          var pricing = project.pricing
+          var rendered = compiled(
+            {
+              id: projectID,
+              description: project.description,
+              url: project.homepage,
+              retracted: project.retracted,
+              name: licensor.name,
+              jurisdiction: licensor.jurisdiction,
+              publicKey: licensor.publicKey,
+              private: pricing.private,
+              relicense: pricing.relicense,
+              lock: pricing.lock
+            },
+            { renderJurisdiction, formatDate, formatPrice }
+          )
+        } catch (error) {
+          return internalError(request, response, error)
+        }
+        sendHTML({ customized: rendered })
+      }
+    )
   })
+
+  function sendHTML (data) {
+    response.setHeader('Content-Type', 'text/html; charset=UTf-8')
+    if (data.customized) {
+      response.end(data.customized)
+    } else {
+      var licensor = data.licensor
+      var project = data.project
+      response.end(html`
+  <!doctype html>
+  <html lang=EN>
+    ${head(projectID, {
+      title: licensor.name + '’s Project',
+      description: project.description
+    })}
+    <body>
+      ${nav()}
+      ${header()}
+      <main>
+        <h2>Project</h2>
+        <section>
+          <dl>
+            <dt>Description</dt>
+            <dd class=description>${escape(project.description)}</dd>
+            <dt>Homepage</dt>
+            <dd class=homepage>
+              <a href="${escape(project.homepage)}" target=_blank>
+                ${escape(project.homepage)}
+              </a>
+            </dd>
+            <dt>Project ID</dt>
+            <dd>
+              <code class=projectID>${escape(projectID)}</code>
+              <button class=clipboard data-clipboard-text="${escape(projectID)}">Copy to Clipboard</button>
+            </dd>
+          </dl>
+        </section>
+        <section>
+          <h3>Licensor</h3>
+          <dl>
+            <dt>Name</dt>
+            <dd>${escape(licensor.name)}</dd>
+            <dt>Jurisdiction</dt>
+            <dd>${renderJurisdiction(licensor.jurisdiction)}</dd>
+            <dt>Public Signing Key</dt>
+            <dd>
+              <pre><code>${licensor.publicKey.slice(0, 32) + '\n' + licensor.publicKey.slice(32)}</code></pre>
+              <button class=clipboard data-clipboard-text="${escape(licensor.publicKey)}">Copy to Clipboard</button>
+            </dd>
+          </dl>
+        </section>
+        <h3>Pricing</h3>
+        ${project.retracted ? retracted() : priceList(project)}
+        ${orderForm(project)}
+      </main>
+      ${footer()}
+      <script src=/clipboard.min.js></script>
+      <script>new ClipboardJS('.clipboard')</script>
+    </body>
+  </html>
+      `)
+    }
+  }
 }
 
 function retracted () {
