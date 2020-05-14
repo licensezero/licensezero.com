ad85bcaa14a1d6e3b7ba39e06378ee4473306a42
Kyle E. Mitchell <kyle@kemitchell.com>
2020-05-13 21:00:57 -0700

    Badges: Do not show relicense prices

--- routes/badge.js
+++ routes/badge.js
@@ -5,10 +5,6 @@ var path = require('path')
 var readOffer = require('../data/read-offer')
 var withCached = require('../data/with-cached')
 
-var withRelicense = withCached(
-  path.join(__dirname, '..', 'static', 'badge-with-relicense.svg')
-)
-
 var withoutRelicense = withCached(
   path.join(__dirname, '..', 'static', 'badge-without-relicense.svg')
 )
@@ -26,9 +22,7 @@ module.exports = function (request, response) {
       return response.end()
     }
     var pricing = offer.pricing
-    var hasRelicense = pricing.relicense
-    var withTemplate = hasRelicense ? withRelicense : withoutRelicense
-    withTemplate(function (error, template) {
+    withoutRelicense(function (error, template) {
       /* istanbul ignore if */
       if (error) {
         response.statusCode = 500
@@ -36,9 +30,6 @@ module.exports = function (request, response) {
       }
       var svg = template
       svg = svg.replace('{private}', formatPrice(pricing.private))
-      if (hasRelicense) {
-        svg = svg.replace('{relicense}', formatPrice(pricing.relicense))
-      }
       response.setHeader('Content-Type', 'image/svg+xml')
       response.end(svg)
     })
